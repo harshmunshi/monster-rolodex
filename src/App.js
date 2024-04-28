00e5c0660da905ef9090f22cfea589e76004a76c
harshmunshi <harshmunshi03@gmail.com>
2024-04-28 15:56:10 +0200

    [update] move header to div

--- src/App.js
+++ src/App.js
@@ -47,7 +47,13 @@ class App extends Component{
       <div className='App'>
         {
           this.state.monsters.map((monster) => {
-            return <h1 key={monster.id}>{monster.name}</h1>
+            return (
+            <div key={monster.id}>
+              <h1>
+                {monster.name}
+              </h1>
+            </div>
+            )
           })
         }
         {/* ----------- Ineffcient -------------- */}
