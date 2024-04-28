354e9f0e59935d740fe2aa29a6a896b412247ef8
harshmunshi <harshmunshi03@gmail.com>
2024-04-28 14:13:45 +0200

    [new] function  -> class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class App extends Component{
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          Hi Harsh here!!
+          </p>
+          <button>
+            Change Name
+          </button> 
+        </header>
+      </div>
+    );
+  }
+}
+
+
+/* ------------------------------------------------------------------------------- */
+/* function way of writing the apps, moving to class
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Harsh here!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>
+          Change Name
+        </button>
       </header>
     </div>
   );
 }
 
+export default App;
+--------------------------------------------------------------------------- */
 export default App;
