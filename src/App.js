b77f4c94b9534831363d0f72452526e5c790e3d1
harshmunshi <harshmunshi03@gmail.com>
2024-04-28 16:42:59 +0200

    [new] searching and filtering

--- src/App.js
+++ src/App.js
@@ -45,6 +45,19 @@ class App extends Component{
   render() {
     return (
       <div className='App'>
+        <input className='search-box' 
+        type='search' 
+        placeholder='search monster' 
+        onChange={(event) => {
+          console.log(event.target.value);
+          const searchString = event.target.value.toLocaleLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchString);
+          });
+          this.setState(() => {
+            return {monsters: filteredMonsters}
+          })
+        }}/>
         {
           this.state.monsters.map((monster) => {
             return (
